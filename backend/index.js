37cebbb8f502188c9d766cc3607dde5e6d8c764c
Krishna Magar <magarkrishna2564@gmail.com>
2024-08-10 12:05:46 +0530

    initializing the express routes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const app = express()
 
 const port = process.env.PORT || 3001
 
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/authority', require('./routes/authorityRoutes'))
+app.use('/api/disaster', require('./routes/disasterRoutes'))
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
