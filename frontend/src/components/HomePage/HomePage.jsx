0a3378a8016ea198cd97bc0e047e020997b88e88
ayushmahandule <ayushmahandule@gmail.com>
2024-08-11 02:21:15 +0530

    map update

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -5,11 +5,18 @@ import Navbar from '../Navbar/Navbar'
 import './HomePage.css'
 import Weather from './Weather'
 import Alert from './Alerts'
+import Map from './Map'
+
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <div className="relative h-screen w-full">
         <div
           className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"
@@ -20,6 +27,10 @@ const HomePage = () => {
           <div>
             <Weather />
             <Alert />
+            <div>
+            <Map />
+            </div>
+            <br></br>
           </div>
         </div>
       </div>
