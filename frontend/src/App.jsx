dfe9626605d746501daaf2b4ee50df03f0debd51
ayushmahandule <ayushmahandule@gmail.com>
2024-08-11 05:31:56 +0530

    maps integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,6 @@ import DisasterDetail from './components/DisasterDetail'
 import Navbar from './components/Navbar/Navbar'
 import TrainingPage from './components/TrainingPage'
 
-=======
-import Login from './components/authentication/Login'
-import ForgotPassword from './components/authentication/ForgotPassword'
-<<<<<<< HEAD
-import ResetPassword from './components/authentication/resetPassword'
-=======
->>>>>>> ceb016e5e7132b24de4d19a2942690adc2199707
->>>>>>> f8bd992fccd4cea7dd1aeb82e438486190b05e69
 function App() {
   return (
     <BrowserRouter>
@@ -26,14 +18,6 @@ function App() {
 <<<<<<< HEAD
         <Route path="/training" element={<TrainingPage />} />
         <Route path="/disaster/:id" element={<DisasterDetail />} />
-=======
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgotPassword" element={<ForgotPassword />} />
-<<<<<<< HEAD
-        <Route path={'/password/reset/:token'} element={<ResetPassword />} />
-=======
->>>>>>> ceb016e5e7132b24de4d19a2942690adc2199707
->>>>>>> f8bd992fccd4cea7dd1aeb82e438486190b05e69
       </Routes>
     </BrowserRouter>
   )
